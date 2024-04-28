7707c40dbe29a657498b78246da27c7afb04faaf
Tanzim <tanzimhossain2@gmail.com>
2024-04-28 16:24:21 +0600

    feat: Implement retrieval of user's location from browser

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,7 @@
+import LocationDetector from "@/components/LocationDetector";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return <div>
+    <LocationDetector />
+  </div>;
 }
